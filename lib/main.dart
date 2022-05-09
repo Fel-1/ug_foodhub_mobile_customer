6e6c5cbc316275b73dd5249ce2f69af9a7b02d63
Fel-1 <felixreinaldosiong@yahoo.co.id>
2022-05-09 10:37:47 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sizer/sizer.dart';
+import 'package:ug_foodhub/onboarding_page.dart';
+
+void main() {
+  //hide status bar of the application
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
+  runApp(UGFoodHub());
+}
+
+class UGFoodHub extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          theme: ThemeData(fontFamily: "Poppins"),
+          home: OnboardingPage(),
+        );
+      },
+    );
+  }
+}
